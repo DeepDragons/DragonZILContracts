dd5014e25e662297db4223c743dacf9ce7613fbd
Rinat <lich666black@gmail.com>
2021-12-10 13:44:24 +0300

    fixed test arena

--- Arena/test.py
+++ Arena/test.py
@@ -1,3 +1,7 @@
+import os
+
+ten = 10
+
 factor0 = 100
 factor1 = 10000
 factor2 = 1000000
@@ -39,69 +43,93 @@ factor37 = 100000000000000000000000000000000000000000000000000000000000000000000
 factor38 = 1000000000000000000000000000000000000000000000000000000000000000000000000000000
 
 
-def get_gene(genes: int, factor: int, decimal: int):
-  return genes % factor // decimal
+def get_gene(last: int, genes: int, factor: int, decimal: int):
+  return (genes % factor // decimal) + last
 
-def get_attack_gene(genes: int, factor: int, decimal: int, entropy: int):
-  return get_gene(genes, factor, decimal)
+def get_attack_gene(last: int, genes: int, factor: int, decimal: int, entropy: int):
+  gene = get_gene(last, genes, factor, decimal)
+  crit = (entropy // factor) % ten
 
-def get_attack(entropy: int, genes: int):
+  return gene + crit
+
+def get_attack(entropy: int, genes: int, luck: int):
   g0 = genes % factor0
-  g1 = get_attack_gene(genes, factor1, factor0, entropy)
-  g2 = get_attack_gene(genes, factor2, factor1, entropy)
-  g3 = get_attack_gene(genes, factor3, factor2, entropy)
-  g4 = get_attack_gene(genes, factor4, factor3, entropy)
-  g5 = get_attack_gene(genes, factor5, factor4, entropy)
-  g6 = get_attack_gene(genes, factor6, factor5, entropy)
-  g7 = get_attack_gene(genes, factor7, factor6, entropy)
-  g8 = get_attack_gene(genes, factor8, factor7, entropy)
-  g9 = get_attack_gene(genes, factor9, factor8, entropy)
-  g10 = get_attack_gene(genes, factor10, factor9, entropy)
-  g11 = get_attack_gene(genes, factor11, factor10, entropy)
-  g12 = get_attack_gene(genes, factor12, factor11, entropy)
-  g13 = get_attack_gene(genes, factor13, factor12, entropy)
-  g14 = get_attack_gene(genes, factor14, factor13, entropy)
-  g15 = get_attack_gene(genes, factor15, factor14, entropy)
-  g16 = get_attack_gene(genes, factor16, factor15, entropy)
-  g17 = get_attack_gene(genes, factor17, factor16, entropy)
-  g18 = get_attack_gene(genes, factor18, factor17, entropy)
-  g19 = get_attack_gene(genes, factor19, factor18, entropy)
-  g38 = get_attack_gene(genes, factor38, factor37, entropy) #TODO luck gene
+  g1 = get_attack_gene(g0, genes, factor1, factor0, entropy)
+  g2 = get_attack_gene(g1, genes, factor2, factor1, entropy)
+  g3 = get_attack_gene(g2, genes, factor3, factor2, entropy)
+  g4 = get_attack_gene(g3, genes, factor4, factor3, entropy)
+  g5 = get_attack_gene(g4, genes, factor5, factor4, entropy)
+  g6 = get_attack_gene(g5 ,genes, factor6, factor5, entropy)
+  g7 = get_attack_gene(g6, genes, factor7, factor6, entropy)
+  g8 = get_attack_gene(g7, genes, factor8, factor7, entropy)
+  g9 = get_attack_gene(g8, genes, factor9, factor8, entropy)
+  g10 = get_attack_gene(g9, genes, factor10, factor9, entropy)
+  g11 = get_attack_gene(g10, genes, factor11, factor10, entropy)
+  g12 = get_attack_gene(g11, genes, factor12, factor11, entropy)
+  g13 = get_attack_gene(g12, genes, factor13, factor12, entropy)
+  g14 = get_attack_gene(g13, genes, factor14, factor13, entropy)
+  g15 = get_attack_gene(g14, genes, factor15, factor14, entropy)
+  g16 = get_attack_gene(g15, genes, factor16, factor15, entropy)
+  g17 = get_attack_gene(g16, genes, factor17, factor16, entropy)
+  g18 = get_attack_gene(g17, genes, factor18, factor17, entropy)
+  g19 = get_attack_gene(g18, genes, factor19, factor18, entropy)
 
-  return (g0, g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19)
+  entropy_number = entropy % factor0
+
+  if entropy_number <= luck:
+    value = g19 * entropy_number // factor0
+    return g19 + value
+
+  return g19
 
 def get_defence_genes(genes: int, defence: bool, percent: int):
-  g20 = get_gene(genes, factor20, factor19)
-  g21 = get_gene(genes, factor21, factor20)
-  g22 = get_gene(genes, factor22, factor21)
-  g23 = get_gene(genes, factor23, factor22)
-  g24 = get_gene(genes, factor24, factor23)
-  g25 = get_gene(genes, factor25, factor24)
-  g26 = get_gene(genes, factor26, factor25)
-  g27 = get_gene(genes, factor27, factor26)
-  g28 = get_gene(genes, factor28, factor27)
-  g29 = get_gene(genes, factor29, factor28)
-  g30 = get_gene(genes, factor30, factor29)
-  g31 = get_gene(genes, factor31, factor30)
-  g32 = get_gene(genes, factor32, factor31)
-  g33 = get_gene(genes, factor33, factor32)
-  g34 = get_gene(genes, factor34, factor33)
-  g35 = get_gene(genes, factor35, factor34)
-  g36 = get_gene(genes, factor36, factor35)
-  g37 = get_gene(genes, factor37, factor36)
+  g20 = get_gene(0, genes, factor20, factor19)
+  g21 = get_gene(g20, genes, factor21, factor20)
+  g22 = get_gene(g21, genes, factor22, factor21)
+  g23 = get_gene(g22, genes, factor23, factor22)
+  g24 = get_gene(g23, genes, factor24, factor23)
+  g25 = get_gene(g24, genes, factor25, factor24)
+  g26 = get_gene(g25, genes, factor26, factor25)
+  g27 = get_gene(g26, genes, factor27, factor26)
+  g28 = get_gene(g27, genes, factor28, factor27)
+  g29 = get_gene(g28, genes, factor29, factor28)
+  g30 = get_gene(g29, genes, factor30, factor29)
+  g31 = get_gene(g30, genes, factor31, factor30)
+  g32 = get_gene(g31, genes, factor32, factor31)
+  g33 = get_gene(g32, genes, factor33, factor32)
+  g34 = get_gene(g33, genes, factor34, factor33)
+  g35 = get_gene(g34, genes, factor35, factor34)
+  g36 = get_gene(g35, genes, factor36, factor35)
+  g37 = get_gene(g36, genes, factor37, factor36)
 
-  return (g20, g21, g22, g23, g24, g25, g26, g27, g28, g29, g30, g31, g32, g33, g34, g35, g36, g37)
+  if defence:
+    increase = g37 * percent // factor0
+    return g37 + increase
 
-def fight(genes: int):
-  attack = get_attack(0, genes)
-  defence = get_defence_genes(genes, False, 10)
-  luck = get_gene(genes, factor38, factor37)
+  return g37
 
-  print({
+def get_dragon_stats(genes: int, defence: int, percent: int):
+  b32 = os.urandom(32)
+  entropy_number = int.from_bytes(b32, "big")
+
+  luck = get_gene(0, genes, factor38, factor37)
+  attack = get_attack(entropy_number, genes, luck)
+  defence = get_defence_genes(genes, defence, percent)
+
+  return {
     'attack': attack,
-    'defence': defence,
-    'luck': luck
-  })
+    'defence': defence
+  }
 
+def start_fight(defender: int, attacker: int):
+  percent = 30
+  dragon0 = get_dragon_stats(defender, True, percent)
+  dragon1 = get_dragon_stats(attacker, False, percent)
 
-fight(94971868430698218815550784762969701691290030012016199912601261219107712080824)
+  print(dragon0)
+  print(dragon1)
+
+start_fight(
+  93874527286910525471527470589474811999999999999999999999999999999999999999953,
+  52715327613880199885229470589684616999999999999999999999999999999999999999961
+)
